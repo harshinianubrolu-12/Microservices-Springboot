9bd599dec131f08e423f2866d93591e38a203272
harshinianubrolu-12 <anubroluharshini2002@gmail.com>
2024-08-28 11:09:53 -0500

    Added logic to create account for a new customer. Handled custom exceptions if customer already exists.

--- accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -1,7 +1,31 @@
 package com.eazybytes.accounts.controller;
 
+import com.eazybytes.accounts.constants.AccountsConstants;
+import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.dto.ResponseDto;
+import com.eazybytes.accounts.exception.CustomerAlreadyExistsException;
+import com.eazybytes.accounts.service.IAccountsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AccountsController {
+@RequestMapping(value = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+public class AccountsController{
+    @Autowired
+    IAccountsService accountsService;
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto)
+    {
+        accountsService.create(customerDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
+
+     }
 }
---
+++ accounts/src/main/java/com/eazybytes/accounts/entity/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.eazybytes.accounts.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Data
+public class BaseEntity {
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+    @Column(updatable = false)
+    private String createdBy;
+    @Column(insertable = false)
+    private String updatedBy;
+}
